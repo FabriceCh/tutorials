fd9d7adbb9bc8649f5b69368cc9e3404de3b8cc0
Fabrice Charbonneau <fabrice.charbonneau@hotmail.com>
2019-08-22 16:08:53 -0400

    tensorflow basic classification

---
+++ tensorflow/basicClassification.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# TensorFlow and tf.Keras
+import tensorflow as tf
+from tensorflow import Keras
+
+# Helper libraries
+import numpy as np
+import matplotlib as plt
+
+print(tf.__version__)
+
